ccf7cfc20e81a7c8dd9196b359914611c2be1295
lby132 <quddbscjswow@naver.com>
2022-03-11 00:04:37 +0900

    JdbcTemplate 사용, jpa 설정

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,11 +1,11 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -14,7 +14,7 @@ public class SpringConfig {
 
     private final DataSource dataSource;
 
-    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(DataSource dataSource, JdbcTemplate jdbcTemplate) {
         this.dataSource = dataSource;
     }
 
@@ -26,6 +26,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
       //  return new MemoryMemberRepository();
-          return new JdbcMemberRepository(dataSource);
+      //    return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -31,5 +32,25 @@ public class MemberServiceIntegrationTest {
         final Member findMember = memberRepository.findById(saveId).get();
         System.out.println("findMember = " + findMember);
         org.junit.jupiter.api.Assertions.assertEquals(member.getName(), findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        final Member member1 = new Member();
+        member1.setName("spring");
+
+        final Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () ->
+                memberService.join(member2)); //예외가 발생해야 한다.
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원 입니다.");
+
+
+
     }
 }
